525222135cf0380921a9d8575e4c8a1751a97f13
Matias Rivas <rivasleandromatias@gmail.com>
2018-10-08 14:20:57 -0300

    fft y fftshift agregados

---
+++ fft.py
@@ -0,0 +1,37 @@
+from cmath import exp, pi
+import numpy as np
+
+#https://rosettacode.org/wiki/Fast_Fourier_transform#Python
+def fft(x):
+    N = len(x)
+    if N <= 1: return x
+    even = fft(x[0::2])
+    odd =  fft(x[1::2])
+    T= [exp(-2j*pi*k/N)*odd[k] for k in range(N//2)]
+    return [even[k] + T[k] for k in range(N//2)] + \
+           [even[k] - T[k] for k in range(N//2)]
+
+#author: matias
+def fftshift(x):
+    N = len(x)
+    par = True if len(x)%2 == 0 else False
+
+    if par:
+        half_size = N//2
+        left = x[half_size:]
+        right = x[:half_size]
+    else:
+        half_size = N//2 + 1
+        left = x[half_size:]
+        right = x[:half_size]
+
+    return np.append(left, right)
+#[1,2,3,4,5][len([1,2,3,4,5])//2:] -> tengo la segunda mitad.
+# len([1,2,3,4,5,6])//2 #me devuelve el floor de la division.
+# len([1,2,3,4,5,6])%2 #me retorna el modulo de la operacion.
+
+
+print(' '.join("%5.3f" % abs(f)
+               for f in fft([1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0])))
+
+
--- heartrate.py
+++ heartrate.py
@@ -8,8 +8,9 @@ Created on Sat Sep 16 19:23:10 2017
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import fft as myfft
 
-cap = cv2.VideoCapture('2017-09-14 21.53.59.mp4')
+cap = cv2.VideoCapture('videos/2017-09-14 21.53.59.mp4')
 
 #if not cap.isOpened(): 
 #    print("No lo pude abrir")
@@ -47,6 +48,16 @@ f = np.linspace(-n/2,n/2-1,n)*fps/n
 r = r[0,0:n]-np.mean(r[0,0:n])
 g = g[0,0:n]-np.mean(g[0,0:n])
 b = b[0,0:n]-np.mean(b[0,0:n])
+
+print(np.fft.fftshift([1,2,3,4,5,6]))
+print('---------------------')
+
+#len([1,2,3,4,5,6])//2 #me devuelve el floor de la division.
+#len([1,2,3,4,5,6])%2 #me retorna el modulo de la operacion.
+#con estas dos operaciones podria crear el fftshift.
+
+print(np.fft.fftshift([1,2,3,4,5]))
+print(myfft.fftshift([1,2,3,4,5]))
 
 R = np.abs(np.fft.fftshift(np.fft.fft(r)))**2
 G = np.abs(np.fft.fftshift(np.fft.fft(g)))**2
