a95f52f7c241c7ccbe72ae5925aa33d4704232d2
santiago <scamisay@itba.edu.ar>
2018-11-02 04:31:25 -0300

    segunda version, falta agregar duracion variable

--- main.py
+++ main.py
@@ -10,6 +10,17 @@ def yaml_loader(file_path):
     return data
 
 
+def frame_selection(position, W, H, w):
+    M = [
+            ["NW","N","NE"],
+            ["W","C","E"],
+            ["SW","S","SE"]
+        ]
+    for i in range(len(M)):
+        for j in range(len(M[i])):
+            if(position == M[i][j]):
+                return int(1/3*i*W), int(1/3*i*W+w), int(1/3*j*H), int(1/3*j*H+w)
+
 def process_file(filename, heartbeats, duration, size, position):
     #print(filename, heartbeats, duration, size, position)
     cap = cv2.VideoCapture('videos/'+filename)
@@ -28,18 +39,19 @@ def process_file(filename, heartbeats, duration, size, position):
     b = np.zeros((1, length))
 
     k = 0
-    while (cap.isOpened()):
-        ret, frame = cap.read()
+    while (cap.isOpened() and r.size > k):
 
+        ret, frame = cap.read()
         width = frame.shape[0]
         height = frame.shape[1]
         frame_size = int(min(width, height*size/100))
+        wmin,wmax,hmin,hmax = frame_selection(position, width, height, frame_size)
         #por lo pronto asumo posicion noroeste
 
         if ret == True:
-            r[0, k] = np.mean(frame[0:frame_size, 0:frame_size, 0])
-            g[0, k] = np.mean(frame[0:frame_size, 0:frame_size, 1])
-            b[0, k] = np.mean(frame[0:frame_size, 0:frame_size, 2])
+            r[0, k] = np.mean(frame[wmin:wmax, hmin:hmax, 0])
+            g[0, k] = np.mean(frame[wmin:wmax, hmin:hmax, 1])
+            b[0, k] = np.mean(frame[wmin:wmax, hmin:hmax, 2])
         #        print(k)
         else:
             break
